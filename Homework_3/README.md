e01b001562a23a0cea605eab5646572582dd6308
David Sharma <david.sharma@gmx.de>
2019-10-28 13:04:59 +0800

    updated readme

---
+++ Homework_3/README.md
@@ -0,0 +1,7 @@
+# Homework_3 
+Homework Description: https://hackmd.io/W8AsZwqeQ5uiSxy5hKV6zA
+
+### How to run the code
+a) Just download the .py file and compile it with "py homework3.py". 
+or b) run the program code with the python online compiler : https://www.onlinegdb.com/online_python_compiler 
+
--- Homework_3/homework3.py
+++ Homework_3/homework3.py
@@ -1,5 +1,6 @@
 #@author David_Sharma_A08922101
 
+
 def calculateEdges(a,b,c,d,e,f,g):
  sumEdges = (a+b+c+d+f+g) + (a*e) + (b*e) + (c*e) + (c*f) + (d*f) + (e*g)
  return sumEdges
@@ -8,7 +9,8 @@ def calculatePaths(a,b,c,d,e,f,g):
  sumPaths = g*(a*e+b*e+c*e)+c*f+d*f
  return sumPaths
 
-a,b,c,d,e,f,g = [int(item) for item in input("Please enter the number of nodes for the seven clusters, sperated by an ',': ").split(",")]
+# read the user's input
+a,b,c,d,e,f,g = [int(nodes) for nodes in input("Please enter the number of nodes for the seven clusters, sperated by an ',' (seven numbers): ").split(",")]
 
 print("Number of edges:3 ", calculateEdges(a,b,c,d,e,f,g))
 print("Number of paths: ", calculatePaths(a,b,c,d,e,f,g))
